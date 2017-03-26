9d4ccefb890fdf366912b35ecb0381919a32c22d
Vignesh Periasami <vikki.logs@gmail.com>
2017-03-26 16:23:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Hackito Eslint rules
+--------------------
+
+Eslint rules for hackito projects.
+
+Usage
+-----
+
+```
+npm install @hackito/eslint-config-default-rules --save-dev
+```
+
+```json
+ "extends": "default-rules"
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "rules": {
+    "brace-style": [
+      2,
+      "1tbs",
+      {
+        "allowSingleLine": true
+      }
+    ],
+    "indent": ["error", 2],
+    "max-len": ["error", 100],
+    "no-trailing-spaces": 2,
+    "arrow-body-style": ["error", "always"],
+    "arrow-parens": ["error", "always"],
+    "arrow-spacing": ["error"],
+    "prefer-const": "error",
+    "space-before-blocks": "error",
+    "space-before-function-paren": ["error", "never"]
+  }
+};
